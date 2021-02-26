51fab0f04d590d727ca3b88e6e981fb0f0fd0a91
Aaron Zuspan <aa.zuspan@gmail.com>
2021-02-26 15:49:08 -0800

    Update description

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="Ecological electivity indices",  # Optional
+    description="Ecological electivity and forage indices",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
